17734b21487c4e39fb1dedb8ac8a879d8f52280f
Adrian Miranda <adrian.miranda137@gmail.com>
2025-07-29 23:38:34 -0600

    Responsive

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css'
 import { NavLink } from "react-router-dom";
+import { FaBars } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
 
 const navBarOptions = [
     {
@@ -31,22 +33,37 @@ const navBarOptions = [
 ]
 
 const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleShowMenu = () => {
+        setShowMenu(!showMenu)
+    }
+
     return(
         <div className="navbar-container">
             <NavLink className="navbar-logo" to='/'>
                 Richard School
             </NavLink>
-            <nav className="navbar-options">
+
+            <i
+                id="menu-icon"
+                onClick={handleShowMenu}
+            >
+                {showMenu ? <FaTimes /> : <FaBars/>}
+            </i>
+
+            {showMenu && <nav className="navbar-options">
                 {navBarOptions.map(({text, to, icon}) => (
                     <NavLink 
                         className="navbar-link" 
                         activeclassname="active" 
+                        onClick={handleShowMenu}
                         to={to} 
                         key={to}>
                         {text}
                     </NavLink>
                 ))}
-            </nav>
+            </nav>}
         </div>
     );
 }
